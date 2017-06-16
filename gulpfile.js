a9bf8150ebcd07e4f80df1d28185bf8cda507441
Milosz Nowicki <milosz.mily.nowicki@gmail.com>
2017-06-16 23:01:40 +0200

    page is cross-browser now

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer =require('gulp-autoprefixer');
 gulp.task('comp', function(){
   return gulp.src('scss/*scss')
   .pipe(sourcemaps.init())
   .pipe(sass({errLogToConsole:true}))
   .pipe(sass({outputStyle:'expanded'}))
+  .pipe(autoprefixer())
   .pipe(sourcemaps.write())
   .pipe(gulp.dest('css'));
 })
